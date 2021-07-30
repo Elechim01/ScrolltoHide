fe0348ffc07cafd63e25566067e854f1a465c5ac
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-30 22:57:59 +0200

    Initial Commit

---
+++ Shared/ScrolltoHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrolltoHideApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 30/07/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrolltoHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
